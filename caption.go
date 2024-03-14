1b373803111537c159bb88a5b30559d34eaf7c5f
Luke Heath <luke@fleetdm.com>
2024-03-14 12:23:26 -0500

    Adjust prompt

--- caption.go
+++ caption.go
@@ -12,13 +12,13 @@ import (
 var openaiAuthToken = os.Getenv("OPENAI_AUTH_TOKEN")
 var defaultCaption = "Much like the cuteness of animals is constant, the number π is a mathematical constant that is the ratio of a circle's circumference to its diameter."
 var chatgptPrompt = "You are an app called qtpi. Your purposes is to interact with users via SMS and share cute animal photos and facts about pi. You are being released on PiDay, March 14, 2024."
-var searchPrompt = "You receive one string: the message the user sent to you. You respond with a single search term for a cute animal photo with no quotes. If the user mentions a specific animal, try to include that animal name in the search term. Your response will be used verbatim to search an API so it must be a single query. This is a kid-friendly service and all search terms should be kid-friendly. If the user sends an inappropriate message, simply ignore it and return a unique search term about animals."
+var searchPrompt = "You receive one string: the message the user sent to you. You respond with a single search term for a cute animal photo with no quotes. If the user mentions a specific animal, try to include that animal name in the search term. Your response will be used verbatim to search an API so it must be a single query. This is a kid-friendly service and all search terms should be kid-friendly. If the user sends an inappropriate message, simply ignore it and return a search term about a specific type of animal."
 var captionPrompt = `You receive two strings that you will use to form your response: 
 
 1. A caption for a photo of a cute animal that will be sent to the user. 
 2. The message the user sent to you.
 
-You respond with a caption for the photo that includes interesting facts and history about pi. Try to include something personal based on the user's message if it is appropriate for this kid-friendly service. If the message is inappropriate, tell the user you don't appreciate that. The target audience are people who enjoy math and are familiar with pi. Try to include a fun fact about the animal described. Try to keep captions sucinct. Emojis are encouraged! If the message from the user is poor quality, prompt them for their favorite animal.`
+You respond with a caption for the photo that includes interesting facts and history about pi. Try to include something personal based on the user's message if it is appropriate for this kid-friendly service. If the message is inappropriate, tell the user you don't appreciate that. The target audience are people who enjoy math and are familiar with pi. Try to include a fun fact about the animal described. Try to keep captions sucinct. Emojis are encouraged! If the message from the user isn't about math or animals, prompt them for their favorite animal.`
 
 func getSearchTerm(message string) (string, error) {
 	client := openai.NewClient(openaiAuthToken)
